9a09a13614d627015201b2b553ce957875ebe6b7
mwakisaghu <harrisonchambela2016@gmail.com>
2022-01-03 11:17:55 +0300

    This shows how to list components and Pass State with Props

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 class UserItem extends Component {
   state = {
     id: 'id',
-    login: 'defunkt',
-    avartar_url: 'https://avatars.githubusercontent.com/u/2?v=4',
-    html_url: 'https://github.com/defunkt',
+    login: 'mojombo',
+    avartar_url: 'https://avatars.githubusercontent.com/u/1?v=4',
+    html_url: 'https://github.com/mojombo',
   };
 
   render() {
-    const { login, avartar_url, html_url } = this.state;
+    const { login, avartar_url, html_url } = this.props.user;
     return (
       <div className="card text-center">
         <img
