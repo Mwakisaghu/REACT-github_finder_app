94a3e5e1208f6bca3812cd2e4128337def4394af
mwakisaghu <harrisonchambela2016@gmail.com>
2022-01-06 01:12:21 +0300

    Code destructuring

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [] /*loading: false */ });
 
   render() {
+    const { users, loading } = this.state;
     return (
       <div className="App">
         <Navbar />
@@ -38,9 +39,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length ? true : false}
+            showClear={users.length ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
