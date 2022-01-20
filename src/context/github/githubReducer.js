85c2fbc084e3d96bdd53e28ad84e6b94e0a9fae7
mwakisaghu <harrisonchambela2016@gmail.com>
2022-01-20 12:52:56 +0300

    This shows how to Create Reducer and reducer actions

---
+++ src/context/github/githubReducer.js
@@ -0,0 +1,26 @@
+import Users from '../../components/users/Users';
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS,
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+};
